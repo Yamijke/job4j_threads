878514cc78c82d90f243e93a513e576e3316c61f
Yamijke <serhei.knyazev@gmail.com>
2024-01-07 17:29:26 +0300

    6. Прерывание блокированной нити. [#267413]

--- src/main/java/concurrent/ConsoleProgress.java
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -17,7 +17,7 @@ public class ConsoleProgress implements Runnable {
             try {
                 Thread.sleep(500);
             } catch (InterruptedException ex) {
-                throw new RuntimeException(ex);
+                Thread.currentThread().interrupt();
             }
         }
     }

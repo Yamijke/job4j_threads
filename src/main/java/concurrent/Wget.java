95a7370f448cd45fc9108c23b35b11100f6eba70
Yamijke <serhei.knyazev@gmail.com>
2024-01-09 17:21:05 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -1,20 +1,51 @@
 package concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i <= 100; i++) {
-                            System.out.print("\rLoading : " + i + "%");
-                            Thread.sleep(1000);
-                        }
-                        System.out.println("Loaded.");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        var file = new File("tmp.xml");
+        try (var input = new URL(url).openStream();
+             var output = new FileOutputStream(file)) {
+            var dataBuffer = new byte[1024];
+            var checkTime = dataBuffer.length * 1000 / speed;
+            int bytesRead;
+            while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                output.write(dataBuffer, 0, bytesRead);
+                long elapsedTime = System.nanoTime() - downloadAt;
+                long elapsedTimeMillis = TimeUnit.NANOSECONDS.toMillis(elapsedTime);
+                if (elapsedTimeMillis < checkTime) {
+                    Thread.sleep(checkTime - elapsedTimeMillis);
                 }
-        );
-        thread.start();
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2 || Integer.parseInt(args[1]) < 0) {
+            System.out.println("Check the arguments");
+            System.exit(0);
+        }
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }

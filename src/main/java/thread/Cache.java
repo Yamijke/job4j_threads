c0820acbb61ea6c0239025250c337da487763599
Yamijke <serhei.knyazev@gmail.com>
2024-01-23 15:41:29 +0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/thread/Cache.java
@@ -0,0 +1,12 @@
+package thread;
+
+public class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache getInstance() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
